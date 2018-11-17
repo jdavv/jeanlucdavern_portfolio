a1147798b5d3981cfce1dc52d112b530482d5a56
jdavv <davernjeanluc@gmail.com>
2018-11-17 14:58:31 -0600

    refactored styling

--- functional_test.py
+++ functional_test.py
@@ -1,22 +1,17 @@
-import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
 
 
 class TestSiteTitle:
-    
     @pytest.fixture
     def driver_setup(self):
-
         mobile_emulation = {"deviceName": "iPhone 6/7/8 Plus"}
         chrome_options = Options()
         chrome_options.add_experimental_option("mobileEmulation",
                                                mobile_emulation)
         self.driver = webdriver.Chrome(options=chrome_options)
-
         yield self.driver
-
         self.driver.quit()
 
     def test_site_is_reachable(self, driver_setup):
