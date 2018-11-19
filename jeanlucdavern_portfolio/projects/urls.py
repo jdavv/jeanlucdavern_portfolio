35e3ffd878bec3a7abcf9bdd89eafe2722e4f2ff
jdavv <davernjeanluc@gmail.com>
2018-11-19 12:20:34 -0600

    create project urls

--- jeanlucdavern_portfolio/projects/urls.py
+++ jeanlucdavern_portfolio/projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 app_name = "projects"
 urlpatterns = [
-    path('', views.index, name='index'),
+        path('', view=views.ProjectsListView.as_view(), name='list'),
+        path('<int:pk>', view=views.ProjectDetailView.as_view(), name='detail')
 ]
