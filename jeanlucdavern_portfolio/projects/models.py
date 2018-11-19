55484c22a5166546fc9214805457537533c8932f
jdavv <davernjeanluc@gmail.com>
2018-11-19 11:54:50 -0600

    Project model created passes test

--- jeanlucdavern_portfolio/projects/models.py
+++ jeanlucdavern_portfolio/projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    repo = models.URLField()
+
+    def __str__(self):
+        return self.title
+
