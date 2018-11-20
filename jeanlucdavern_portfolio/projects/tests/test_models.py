c4e01e608e75eea17693fd720e002afe86991027
jdavv <davernjeanluc@gmail.com>
2018-11-20 11:24:13 -0600

    created failing unit test for slug method on project model

--- jeanlucdavern_portfolio/projects/tests/test_models.py
+++ jeanlucdavern_portfolio/projects/tests/test_models.py
@@ -1,5 +1,6 @@
 import pytest
-
+from django.template.defaultfilters import slugify
+from mixer.backend.django import mixer
 from jeanlucdavern_portfolio.projects.models import Project
 
 pytestmark = pytest.mark.django_db
@@ -9,3 +10,11 @@ class TestModels:
     def test_get_string_repr(self):
         project = Project(title="this title")
         assert str(project) == "this title"
+
+    def test_model(self):
+        obj = mixer.blend('projects.Project')
+        assert obj.pk == 1, 'Should create a Project instance'
+
+    def test_project_title_is_slugified(self):
+        proj = mixer.blend('projects.Project')
+        assert proj.slug == slugify(proj.title), 'Slug should pre-populate from title'
