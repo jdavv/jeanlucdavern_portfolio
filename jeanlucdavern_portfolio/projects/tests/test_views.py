0df83dfd2746d6c04c1fe839ee5b5b374041e070
jdavv <davernjeanluc@gmail.com>
2018-11-20 11:05:32 -0600

    added project list view test

--- jeanlucdavern_portfolio/projects/tests/test_views.py
+++ jeanlucdavern_portfolio/projects/tests/test_views.py
@@ -3,17 +3,20 @@ from django.urls import reverse
 from jeanlucdavern_portfolio.projects.views import ProjectsListView, ProjectDetailView
 from jeanlucdavern_portfolio.projects.models import Project
 import pytest
+from mixer.backend.django import mixer
 
 
 @pytest.mark.django_db
 class TestViews:
     def test_project_detail(self):
-        self.project = Project.objects.create(
-            title='title',
-            description='description is descriptive',
-            repo='http://test.com')
+        proj = mixer.blend('projects.Project')
         path = reverse('projects:detail', kwargs={'pk': 1})
         request = RequestFactory().get(path)
+        response = ProjectDetailView.as_view()(request, pk=proj.pk)
+        assert response.status_code == 200
 
-        response = ProjectDetailView.as_view()(request, pk=1)
+    def test_project_list(self):
+        path = reverse('projects:list')
+        request = RequestFactory().get(path)
+        response = ProjectsListView.as_view()(request)
         assert response.status_code == 200
