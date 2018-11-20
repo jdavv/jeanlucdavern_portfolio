660dc988927e3286e262301fc21c85f085e5b159
jdavv <davernjeanluc@gmail.com>
2018-11-20 12:31:52 -0600

    Override save method to slugify title

--- jeanlucdavern_portfolio/projects/models.py
+++ jeanlucdavern_portfolio/projects/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Project(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=128)
     repo = models.URLField()
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Project, self).save(*args, **kwargs)
+            
