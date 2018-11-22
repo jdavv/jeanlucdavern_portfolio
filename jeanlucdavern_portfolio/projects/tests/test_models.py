68c7c6277a24b63c4d292e18a4911484664c6fbf
jdavv <davernjeanluc@gmail.com>
2018-11-21 20:27:49 -0600

    Created failing test for technologies model

--- jeanlucdavern_portfolio/projects/tests/test_models.py
+++ jeanlucdavern_portfolio/projects/tests/test_models.py
@@ -6,15 +6,21 @@ from jeanlucdavern_portfolio.projects.models import Project
 pytestmark = pytest.mark.django_db
 
 
-class TestModels:
+class TestProjectsModel:
     def test_get_string_repr(self):
         project = Project(title="this title")
         assert str(project) == "this title"
 
-    def test_model(self):
+    def test_can_create_project_object(self):
         obj = mixer.blend('projects.Project')
         assert obj.pk == 1, 'Should create a Project instance'
 
     def test_project_title_is_slugified(self):
         proj = mixer.blend('projects.Project')
         assert proj.slug == slugify(proj.title), 'Slug should pre-populate from title'
+
+
+class TestTechnologiesModel:
+    def test_can_create_technology_object(self):
+        technology = mixer.blend('projects.Technologies')
+        assert technology.pk == 1, 'Should create a Technology instance'
