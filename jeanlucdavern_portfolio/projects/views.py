57b41aca19a86ccdac0aec8f3944ef6999a7e247
jdavv <davernjeanluc@gmail.com>
2018-11-24 11:28:00 -0600

    Create view for TechnologiesListView, override get_queryset

--- jeanlucdavern_portfolio/projects/views.py
+++ jeanlucdavern_portfolio/projects/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from .models import Project
+from django.shortcuts import get_object_or_404
+from .models import Project, Technologies
 
 
 class ProjectsListView(ListView):
@@ -8,3 +9,10 @@ class ProjectsListView(ListView):
 
 class ProjectDetailView(DetailView):
     model = Project
+
+
+class TechnologiesListView(ListView):
+    model = Technologies
+
+    def get_queryset(self):
+        return Technologies.objects.filter(slug=self.request.GET.get('slug'))
