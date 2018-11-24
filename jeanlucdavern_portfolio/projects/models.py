9288b4e112fc591ad295188eb57f1aff665c60a6
jdavv <davernjeanluc@gmail.com>
2018-11-24 11:17:47 -0600

    Create Technologies model. Added relation to Project model.

--- jeanlucdavern_portfolio/projects/models.py
+++ jeanlucdavern_portfolio/projects/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.urls import reverse
+
+
+class Technologies(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Technologies, self).save(*args, **kwargs)
 
 
 class Project(models.Model):
@@ -10,6 +23,7 @@ class Project(models.Model):
     image = models.ImageField(
             upload_to='project_images',
             default='project_images/default_project.png')
+    technologies = models.ManyToManyField(Technologies)
 
     def __str__(self):
         return self.title
@@ -18,6 +32,5 @@ class Project(models.Model):
         self.slug = slugify(self.title)
         super(Project, self).save(*args, **kwargs)
 
-
-class Technologies(models.Model):
-    name = models.CharField(max_length=64)
+    def get_absolute_url(self):
+        return reverse("projects:detail", kwargs={"slug": self.slug})
