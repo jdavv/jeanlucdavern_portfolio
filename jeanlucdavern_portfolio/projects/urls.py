422dec9c9b99c4078507cf7877fff09918f97c38
jdavv <davernjeanluc@gmail.com>
2018-11-25 14:22:43 -0600

    Wire up url conf for /tagged/*

--- jeanlucdavern_portfolio/projects/urls.py
+++ jeanlucdavern_portfolio/projects/urls.py
@@ -6,4 +6,5 @@ app_name = "projects"
 urlpatterns = [
         path('', view=views.ProjectsListView.as_view(), name='list'),
         path('<slug>', view=views.ProjectDetailView.as_view(), name='detail'),
+        path('tagged/<slug>', view=views.TechnologiesListView.as_view(), name='tech_list'),
 ]
