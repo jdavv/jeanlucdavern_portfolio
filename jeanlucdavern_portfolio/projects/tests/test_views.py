894208e73dd79e08bc059201acf2ee99bdf7678c
jdavv <davernjeanluc@gmail.com>
2018-11-25 14:20:24 -0600

    Refacted tests

--- jeanlucdavern_portfolio/projects/tests/test_views.py
+++ jeanlucdavern_portfolio/projects/tests/test_views.py
@@ -2,28 +2,29 @@ import pytest
 from django.test import RequestFactory
 from django.urls import reverse
 from mixer.backend.django import mixer
-
+from ..models import Project
 from jeanlucdavern_portfolio.projects.views import ProjectDetailView, ProjectsListView, TechnologiesListView
 
 
 @pytest.mark.django_db
 class TestProjectViews:
     def test_project_detail_view(self):
-        proj = mixer.blend('projects.Project')
-        path = reverse('projects:detail', kwargs={'slug': proj.slug})
-        request = RequestFactory().get(path)
-        response = ProjectDetailView.as_view()(request, slug=proj.slug)
-        assert response.status_code == 200, 'Should be status code 200'
+        self.proj = mixer.blend('projects.Project')
+        self.path = reverse('projects:detail', kwargs={'slug': self.proj.slug})
+        self.request = RequestFactory().get(self.path)
+        self.response = ProjectDetailView.as_view()(self.request, slug=self.proj.slug)
+        assert self.response.status_code == 200, 'Should be status code 200'
 
     def test_project_list_view(self):
-        path = reverse('projects:list')
-        request = RequestFactory().get(path)
-        response = ProjectsListView.as_view()(request)
-        assert response.status_code == 200, 'Should be status code 200'
+        self.path = reverse('projects:list')
+        self.request = RequestFactory().get(self.path)
+        self.response = ProjectsListView.as_view()(self.request)
+        assert self.response.status_code == 200, 'Should be status code 200'
 
     def test_projects_by_technologies_view(self):
-        tech = mixer.blend('projects.Technologies')
-        path = reverse('projects:detail', kwargs={'slug': tech.slug})
-        request = RequestFactory().get(path)
-        response = TechnologiesListView.as_view()(request)
-        assert response.status_code == 200, 'Should be status code 200'
+        self.tech = mixer.blend('projects.Technologies')
+        self.proj = mixer.blend(Project, technologies=self.tech)
+        self.path = reverse('projects:tech_list', kwargs={'slug': self.tech.slug})
+        self.request = RequestFactory().get(self.path)
+        self.response = TechnologiesListView.as_view()(self.request, slug=self.tech.slug)
+        assert self.response.status_code == 200, 'Should be status code 200'
