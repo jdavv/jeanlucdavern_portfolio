b269272cdba63357fcb4fb69ce879e8fe17e5693
jdavv <davernjeanluc@gmail.com>
2018-12-03 16:54:02 -0600

    Created tests for redirect on valid form

--- jeanlucdavern_portfolio/contact/tests/test_views.py
+++ jeanlucdavern_portfolio/contact/tests/test_views.py
@@ -1,14 +1,27 @@
-from jeanlucdavern_portfolio.contact.views import ContactView
-from django.urls import resolve
+from jeanlucdavern_portfolio.contact.views import ContactView, SuccessView
 from django.test import RequestFactory
+from django.test import Client
+import pytest
 
 
+@pytest.mark.django_db
 class TestContactView:
-    # contact_page = resolve('/contact/')
-    # exepcted = ContactView
-    # assert contact_page.func == exepcted
-
     def test_contact_view(self):
         req = RequestFactory().get('/contact/')
         resp = ContactView.as_view()(req)
-        assert resp.status_code == 200,  'Should be callable by anyone'
+        assert resp.status_code == 200, 'Should be callable by anyone'
+
+    def test_contact_success_view(self):
+        req = RequestFactory().get('/contact/success/')
+        resp = SuccessView.as_view()(req)
+        assert resp.status_code == 200, 'Should be callable by anyone'
+
+    def test_contact_view_redirects_with_valid_form_submitted(self):
+        client = Client()
+        resp = client.post(
+            '/contact/', {
+                'email': 'valid@email.com',
+                'subject': 'a valid subject',
+                'message': 'a valid message'
+            })
+        assert resp.status_code == 301
