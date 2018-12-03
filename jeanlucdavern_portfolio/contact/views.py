42830e3149040bb6537d6b70f6875888e8d9bfc5
jdavv <davernjeanluc@gmail.com>
2018-12-03 16:56:54 -0600

    Created SuccessView

--- jeanlucdavern_portfolio/contact/views.py
+++ jeanlucdavern_portfolio/contact/views.py
@@ -1,7 +1,12 @@
 from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
 from .forms import EmailForm
 
 
 class ContactView(FormView):
     template_name = 'contact/emailform.html'
     form_class = EmailForm
+
+
+class SuccessView(TemplateView):
+    template_name = 'contact/emailsuccess.html'
