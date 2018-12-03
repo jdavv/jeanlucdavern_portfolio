5cddca46d726a3c60c5e36305a3030113f1f2bab
jdavv <davernjeanluc@gmail.com>
2018-12-03 15:27:11 -0600

    Refactored views tests

--- jeanlucdavern_portfolio/contact/tests/test_views.py
+++ jeanlucdavern_portfolio/contact/tests/test_views.py
@@ -1,8 +1,14 @@
 from jeanlucdavern_portfolio.contact.views import ContactView
 from django.urls import resolve
+from django.test import RequestFactory
 
 
-class ContactViewTest:
-    contact_page = resolve('contact/')
-    exepcted = ContactView
-    assert contact_page.func == exepcted
+class TestContactView:
+    # contact_page = resolve('/contact/')
+    # exepcted = ContactView
+    # assert contact_page.func == exepcted
+
+    def test_contact_view(self):
+        req = RequestFactory().get('/contact/')
+        resp = ContactView.as_view()(req)
+        assert resp.status_code == 200,  'Should be callable by anyone'
