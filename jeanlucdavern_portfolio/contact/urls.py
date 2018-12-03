e9d4e1f24031c04a82b438f21416c4e4b7c0a902
jdavv <davernjeanluc@gmail.com>
2018-12-03 17:29:50 -0600

    Add SuccessView to urlpatterns

--- jeanlucdavern_portfolio/contact/urls.py
+++ jeanlucdavern_portfolio/contact/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'contact'
 urlpatterns = [
-    path('', view=views.ContactView.as_view(), name='contact_index')
+    path('', view=views.ContactView.as_view(), name='contact_index'),
+    path('success/', view=views.SuccessView.as_view(), name='contact_success'),
 ]
