68a3ea1f4e100cae0d34ec01ec90db4abfa3d811
jdavv <davernjeanluc@gmail.com>
2018-12-04 17:06:46 -0600

    Add placeholders, and form_valid, method

--- jeanlucdavern_portfolio/contact/forms.py
+++ jeanlucdavern_portfolio/contact/forms.py
@@ -4,12 +4,18 @@ from crispy_forms.layout import Layout, Submit, Column
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(required=True)
-    subject = forms.CharField(max_length=160, required=True)
-    message = forms.CharField(required=True)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Your email'}), required=True)
+    subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Subject'}), max_length=160, required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message'}), required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.form_show_labels = False
         self.helper.layout = Layout(
-            Column('email', 'subject', 'message'), Submit('send', 'send'))
+            Column('email', 'subject', 'message'), Submit('send', 'Send'))
+
+        def form_valid(self, form):
+            print(self.form.cleaned_data['email'])
+            print(self.request.POST.get['subject'])
+            return super(EmailForm, self).form_valid(form)
