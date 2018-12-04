51d50bd245dc1d2560e0d6314d4663b452a68fe5
jdavv <davernjeanluc@gmail.com>
2018-12-03 18:30:00 -0600

    Switch to crispy forms

--- jeanlucdavern_portfolio/contact/forms.py
+++ jeanlucdavern_portfolio/contact/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Column
 
 
 class EmailForm(forms.Form):
     email = forms.EmailField(required=True)
     subject = forms.CharField(max_length=160, required=True)
     message = forms.CharField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Column('email', 'subject', 'message'), Submit('send', 'send'))
