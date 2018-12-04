e2c3dd2212d6a184121617e3963c8d63687cfb5b
jdavv <davernjeanluc@gmail.com>
2018-12-04 17:12:14 -0600

    Added method in the right class and file :)

--- jeanlucdavern_portfolio/contact/views.py
+++ jeanlucdavern_portfolio/contact/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
+from django.core.mail import send_mail
 from .forms import EmailForm
 
 
@@ -7,6 +8,13 @@ class ContactView(FormView):
     template_name = 'contact/emailform.html'
     form_class = EmailForm
     success_url = 'success/'
+
+    def form_valid(self, form):
+        from_email = self.request.POST.get('email')
+        subject = self.request.POST.get('subject')
+        message = self.request.POST.get('message')
+        send_mail(subject, message, from_email, ['admin@jeanlucdavern.com'], fail_silently=True)
+        return super(ContactView, self).form_valid(form)
 
 
 class SuccessView(TemplateView):
