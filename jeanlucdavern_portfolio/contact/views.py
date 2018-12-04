eb519e7866bd6f88e228b9106de5e6197e6ee4b0
jdavv <davernjeanluc@gmail.com>
2018-12-04 17:51:19 -0600

    Change send_mail's recipient list to an env var

--- jeanlucdavern_portfolio/contact/views.py
+++ jeanlucdavern_portfolio/contact/views.py
@@ -2,6 +2,9 @@ from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
 from django.core.mail import send_mail
 from .forms import EmailForm
+import os
+
+RECIPIENT_LIST = os.environ['DJANGO_MAILTO']
 
 
 class ContactView(FormView):
@@ -13,7 +16,7 @@ class ContactView(FormView):
         from_email = self.request.POST.get('email')
         subject = self.request.POST.get('subject')
         message = self.request.POST.get('message')
-        send_mail(subject, message, from_email, ['admin@jeanlucdavern.com'], fail_silently=True)
+        send_mail(subject, message, from_email, [RECIPIENT_LIST], fail_silently=True)
         return super(ContactView, self).form_valid(form)
 
 
