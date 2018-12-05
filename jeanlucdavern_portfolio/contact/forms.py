3a990177173635efa35ead1b48f785cf01433e75
jdavv <davernjeanluc@gmail.com>
2018-12-05 12:28:00 -0600

    Implement Google's Recaptcha v3 into email form

--- jeanlucdavern_portfolio/contact/forms.py
+++ jeanlucdavern_portfolio/contact/forms.py
@@ -1,16 +1,25 @@
-from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Column
+from crispy_forms.layout import HTML, Column, Layout, Submit
+from django import forms
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Your email'}), required=True)
-    subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Subject'}), max_length=160, required=True)
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message'}), required=True)
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={'placeholder': 'Your email'}),
+        required=True)
+    subject = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Subject'}),
+        max_length=160,
+        required=True)
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Message'}), required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_show_labels = False
         self.helper.layout = Layout(
-            Column('email', 'subject', 'message'), Submit('send', 'Send'))
+            Column('email', 'subject', 'message'),
+            HTML(
+                '<div class="g-recaptcha" data-sitekey="6LdeCH8UAAAAAICyVnGPLISBlb5K6ASzSweic8Ob"></div>'
+            ), Submit('send', 'Send', css_class="mt-3"))
--- jeanlucdavern_portfolio/contact/views.py
+++ jeanlucdavern_portfolio/contact/views.py
@@ -1,10 +1,15 @@
-from django.views.generic.edit import FormView
-from django.views.generic import TemplateView
-from django.core.mail import send_mail
-from .forms import EmailForm
 import os
 
+import requests
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+
+from .forms import EmailForm
+
 RECIPIENT_LIST = os.environ['DJANGO_MAILTO']
+RECAPTCHA_SECRET_KEY = os.environ['RECAPTCHA_SECRET_KEY']
 
 
 class ContactView(FormView):
@@ -16,8 +21,26 @@ class ContactView(FormView):
         from_email = self.request.POST.get('email')
         subject = self.request.POST.get('subject')
         message = self.request.POST.get('message')
-        send_mail(subject, message, from_email, [RECIPIENT_LIST], fail_silently=True)
-        return super(ContactView, self).form_valid(form)
+
+        recaptcha_response = self.request.POST.get('g-recaptcha-response')
+        data = {
+            'secret': RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response,
+        }
+
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify', data=data)
+
+        recaptcha_result = recaptcha_request.json()
+        if recaptcha_result['success']:
+            send_mail(
+                subject,
+                message,
+                from_email, [RECIPIENT_LIST],
+                fail_silently=True)
+            return super(ContactView, self).form_valid(form)
+        else:
+            return redirect('/contact/')
 
 
 class SuccessView(TemplateView):
