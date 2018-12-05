be9bc551d849dacc645938b3530471ae0d90c763
jdavv <davernjeanluc@gmail.com>
2018-12-05 15:38:52 -0600

    Add space under Send button for mobile

--- jeanlucdavern_portfolio/contact/forms.py
+++ jeanlucdavern_portfolio/contact/forms.py
@@ -22,4 +22,4 @@ class EmailForm(forms.Form):
             Column('email', 'subject', 'message'),
             HTML(
                 '<div class="g-recaptcha" data-sitekey="6LdeCH8UAAAAAICyVnGPLISBlb5K6ASzSweic8Ob"></div>'
-            ), Submit('send', 'Send', css_class="mt-3"))
+            ), Submit('send', 'Send', css_class="mt-3 mb-3"))
