ca333a1058d8f3ab49c779317ebd6949387950d8
jdavv <davernjeanluc@gmail.com>
2018-12-07 19:43:24 -0600

    Add SharingMetaAdmin

--- jeanlucdavern_portfolio/projects/admin.py
+++ jeanlucdavern_portfolio/projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About, Project, Keywords
+from .models import About, SharingMeta, Project, Keywords
 
 
 class KeywordsAdmin(admin.ModelAdmin):
@@ -18,6 +18,11 @@ class AboutAdmin(admin.ModelAdmin):
     pass
 
 
+class SharingMetaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(SharingMeta, SharingMetaAdmin)
 admin.site.register(About, AboutAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Keywords, KeywordsAdmin)
